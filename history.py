ded532fcfe8ddff4c2e634a3e05eada1f207547e
shreya-dixit <64888042+shreya-dixit@users.noreply.github.com>
2020-06-25 20:24:09 +0530

    Added project in reposirory
    Projected developed offline and added to repository to have global availability

---
+++ history.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Jun  1 11:57:48 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Wed Jun  3 11:49:37 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Thu Jun  4 13:31:56 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Fri Jun  5 19:39:57 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Mon Jun  8 10:26:36 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Mon Jun  8 14:56:21 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Tue Jun  9 11:32:44 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Tue Jun  9 15:41:19 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Tue Jun  9 19:49:29 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Wed Jun 10 15:23:45 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Thu Jun 11 16:32:09 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Fri Jun 12 10:59:21 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Fri Jun 12 14:26:08 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Sun Jun 14 12:18:19 2020)---
+debugfile('F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy/server.py', wdir='F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy')
+
+## ---(Mon Jun 15 11:55:12 2020)---
+debugfile('F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy/server.py', wdir='F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy')
+
+## ---(Mon Jun 15 12:40:20 2020)---
+debugfile('F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy/server.py', wdir='F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy')
+
+## ---(Mon Jun 15 15:14:04 2020)---
+debugfile('F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy/server.py', wdir='F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy')
+
+## ---(Mon Jun 15 19:49:49 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Tue Jun 16 12:23:22 2020)---
+debugfile('F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy/server.py', wdir='F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy')
+
+## ---(Tue Jun 16 16:05:56 2020)---
+debugfile('F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy/server.py', wdir='F:/Shreya/BloodBankPSITProject/Blood-Bank - Copy')
+
+## ---(Sat Jun 20 12:43:21 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Mon Jun 22 11:41:14 2020)---
+runcell(0, 'C:/Users/Gaurav/.spyder-py3/temp.py')
+
+## ---(Mon Jun 22 17:57:49 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Mon Jun 22 19:40:09 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Tue Jun 23 12:41:45 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
+
+## ---(Thu Jun 25 15:48:29 2020)---
+debugfile('C:/Users/Gaurav/.spyder-py3/temp.py', wdir='C:/Users/Gaurav/.spyder-py3')
